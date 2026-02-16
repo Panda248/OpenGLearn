8307914a443eb2246a83703ccd6c3b7eaebb5df8
Panda248 <40081083+Panda248@users.noreply.github.com>
2026-02-15 22:08:27 -0500

    drawing circles rather than ovals

--- shaders/lineTrailVert.glsl
+++ shaders/lineTrailVert.glsl
@@ -5,13 +5,14 @@ layout (location = 1) in float aRadius;
 layout (location = 2) in float aOffset;
 
 uniform float u_time;
+uniform float u_aspect_ratio;
 
 out vec2 FragPos;
 
 void main()
 {
     float t = u_time + aOffset;
-    vec2 pos = aPos + vec2(aRadius * cos(t), aRadius * sin(t));
+    vec2 pos = aPos + vec2(aRadius * cos(t), u_aspect_ratio * aRadius * sin(t));
     gl_Position = vec4(pos, 0.0, 1.0);
     FragPos = gl_Position.xy;
 } 
